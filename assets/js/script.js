52f32d4e3ca3971c99283dceaa5565ec4f88dac1
Indira <ristovskindira@gmail.com>
2025-04-10 00:47:11 +0200

    Fixing js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,44 @@
 const questions = [
     {
-    question: "Which planet is known as the 'Red Planet'?",
+        question: "Which planet is known as the 'Red Planet'?",
     answers: [
         {text: "Earth", correct: false},
-         {text: "Jupiter", correct: false},
+        {text: "Jupiter", correct: false},
         {text: "Saturn", correct: false},
         {text: "Mars", correct: true},
     ]
-},
-{
-    question: "Which of these countries does not have a coastline?",
-    answers: [
-        {text: "Brazil", correct: false},
-        {text: "Mali", correct: true},
-        {text: "Egypt", correct: false},
-        {text: "Argentina", correct: false},
-    ]
-},
-{
-    question: "Which country is the origin of sushi?",
-    answers: [
-        {text: "Japan", correct: true},
-        {text: "China", correct: false},
-        {text: "Thailand", correct: false},
-        {text: "South Korea", correct: false},
-    ]
-},
-{
-    question: "In which movie would you find the character 'Jack Dawson'?",
-    answers: [
-        {text: "The Great Gatsby", correct: false},
-        {text: "Avatar", correct: false},
-        {text: "Inception", correct: false}
-        {text: "Titanic", correct: true},
-    ]
-},
-{
-    question : "Who was the first woman to fly solo across the Atlantic Ocean?",
-    answers: [
-        {text: "Amelia Earhart", correct: true},
-        {text: "Jacqueline Cochran", correct: false},
-        {text: "Bessie Coleman", correct: false}
-        {text: " Sally Ride", correct: false},
-    ]
-},
+    },
+    {
+        question: "Which of these countries does not have a coastline?",
+        answers: [
+            {text: "Brazil", correct: false},
+            {text: "Mali", correct: true},
+            {text: "Egypt", correct: false},
+            {text: "Argentina", correct: false},
+        ]
+    },
+    {
+        question: "Which country is the origin of sushi?",
+        answers: [
+            {text: "Japan", correct: true},
+            {text: "China", correct: false},
+            {text: "Thailand", correct: false},
+            {text: "South Korea", correct: false},
+        ]
+    },
+    {
+        question: "What is the capital of Portugal?",
+        answers: [
+            {text: "Vienna", correct: true},
+            {text: "Madrid", correct: false},
+            {text: "Lisbon", correct: true},
+            {text: "Belgrade", correct: false},
+
+        ]
+    }
+   
 ];
+   
 
 
 const questionElement= document.getElementById("question");
@@ -57,36 +51,38 @@ let score = 0;
 function startGame() {
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "Next";
+    nextButton.innerHTML = "Next Question";
     showQuestion();
 }
     function showQuestion() {
         resetState();
         let currentQuestion= questions[currentQuestionIndex];
-        let questionNo = currentQuestionIndex + 1;
-        questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
-
+        questionElement.innerText = currentQuestion.question;
+        
         currentQuestion.answers.forEach(answer => {
-            const button = document.createElement("button");
-            button.innerHTML = answer.text;
+
+            const button = document.forEach("button");
+            button.innerText = answer.text;
             button.classList.add("btn");
-            answerButton.appendChild(button);
-            button.addEventListener("click", selectAnswer);
             if (answer.correct) {
                 button.dataset.correct = answer.correct;
-            })
+            }
+            button.addEventListener("click",selectAnswer);
+            answerButton.appendChild(button)
         });
-
-
-        function resetState() {
+    }
+    
+    function resetState() {
             nextButton.style.display ="none";
             while (answerButton.firstChild) {
                 answerButton.removeChild(answerButton.firstChild);
             }
         }
+
         function selectAnswer(e) {
             const selectedBtn = e.target;
             const correct = selectedBtn.dataset.correct === "true";
+
             if(correct) {
                 selectedBtn.classList.add("correct");
                 score++;
@@ -104,7 +100,7 @@ function startGame() {
         nextButton.style.display = "block";
         }
 
-        function showScore() {
+        function showResult() {
             resetState();
             questionElement.innerHTML = `You scored  ${score} out of ${questions.length}`; 
             nextButton.innerHTML = "Play Again";
@@ -116,7 +112,7 @@ function startGame() {
             if (currentQuestionIndex < questions.length) {
                 showQuestion();
             } else {
-                showScore();
+                showResult();
             }
         }
 
@@ -126,3 +122,7 @@ function startGame() {
             }
         } )
         startGame();
+
+  
+
+
